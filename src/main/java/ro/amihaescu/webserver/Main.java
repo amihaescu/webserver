4ca1cd19250c2d97d693ac3f29fc6f16820216e6
Andrei Mihaescu <andrei.mihaescu91@gmail.com>
2018-09-15 14:51:17 +0300

    - increased number of threads

--- src/main/java/ro/amihaescu/webserver/Main.java
+++ src/main/java/ro/amihaescu/webserver/Main.java
@@ -5,7 +5,7 @@ import java.util.Date;
 public class Main {
 
     public static void main(String[] args) {
-        Thread thread = new Thread(new Server(8080, 2, "www"));
+        Thread thread = new Thread(new Server(8080, 3, "www"));
         //thread.setDaemon(true);
         thread.start();
         System.out.println(String.format("%s - %s - Started server", Thread.currentThread(), new Date()));
