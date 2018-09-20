6247809aa679b27846ef3dacbe6fd6fd4235f09d
Andrei Mihaescu <amihaescu@deloittece.com>
2018-09-20 14:40:18 +0300

    - rebased and externalized configuration for keep alive connection

--- src/main/java/ro/amihaescu/webserver/Main.java
+++ src/main/java/ro/amihaescu/webserver/Main.java
@@ -7,19 +7,22 @@ public class Main {
     private static final Integer DEFAULT_PORT = 8080;
     private static final Integer DEFAULT_NO_THREADS = 3;
     private static final String DEFAULT_WEB_ROOT = "www";
+    private static final Long DEFAULT_CONNECTION_KEEP_ALIVE_TIME = 5000L;
 
     private static Integer serverPort;
     private static Integer noThreads;
     private static String webRoot;
+    private static Long connectionKeepAliveTime;
 
     public static void main(String[] args) {
 
 
         try {
-            if (args.length == 3) {
+            if (args.length == 4) {
                 serverPort = Integer.valueOf(args[0]);
                 noThreads = Integer.valueOf(args[1]);
                 webRoot = args[2];
+                connectionKeepAliveTime = Long.valueOf(args[3]);
             } else {
                 defaultInit();
             }
@@ -28,11 +31,12 @@ public class Main {
             defaultInit();
         }
 
-        Thread thread = new Thread(new Server(8080, 3, "www", 10_000L));
+        Thread thread = new Thread(new Server(serverPort, noThreads, webRoot, connectionKeepAliveTime));
         thread.start();
         System.out.printf("%s - Server will run on port %s; \n" , new Date(), serverPort);
         System.out.printf("%s - Number of threads  %s; \n" , new Date(), noThreads);
         System.out.printf("%s - The web root for the server is %s; \n" , new Date(), webRoot);
+        System.out.printf("%s - The connection keep alive time is %s; \n" , new Date(), connectionKeepAliveTime);
 
         System.out.printf("%s - Started server \n", new Date());
 
@@ -44,5 +48,6 @@ public class Main {
         serverPort = DEFAULT_PORT;
         noThreads = DEFAULT_NO_THREADS;
         webRoot = DEFAULT_WEB_ROOT;
+        connectionKeepAliveTime = DEFAULT_CONNECTION_KEEP_ALIVE_TIME;
     }
 }
